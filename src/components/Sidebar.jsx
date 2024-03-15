bc0601fb6a1791a1fd43a62f0eb92e2e8c8b2297
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-03-15 22:39:58 +0500

    sidebar progress

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Sidebar() {
-	return <div className=' border border-black h-screen'>SideBar</div>;
+	return (
+		<div className='flex flex-col items-center justify-evenly border border-black h-screen text-white'>
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+		</div>
+	);
 }
 
 export default Sidebar;
