d51264387605a1ffa09a725de32da0f512946c3f
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-03-15 22:53:47 +0500

    sidebar added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import IMG from '../assets/images/profile.png';
 function Sidebar() {
 	return (
-		<div className='flex flex-col items-center justify-evenly border border-black h-screen text-white'>
-			<div>1</div>
-			<div>2</div>
-			<div>3</div>
+		<div className='flex flex-col items-center justify-center border border-black h-screen text-white'>
+			<div>
+				<h1 className='text-3xl text-white font-bold'>HELSB</h1>
+			</div>
+			<div className='flex flex-col items-center justify-center'>
+				<img src={IMG} alt='' width={100} />
+				<h2 className='text-sm font-semibold my-2'>Abdul Samad Khan</h2>
+			</div>
+			<div></div>
 			<div>4</div>
 		</div>
 	);
