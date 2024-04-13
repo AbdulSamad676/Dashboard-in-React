48e3697f6e7c5b98775cf78534e1a180e61142ba
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-04-13 08:48:02 +0500

    userProfile completed

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,20 +6,59 @@ function User() {
       <div className='title-row'>
         <h2 className='text-2xl font-semibold'>User Profile</h2>
       </div>
-      <div className='profile-row grid grid-cols-11 my-5 gap-x-5'>
-        <div className='w-auto flex jusitfy-center flex-col gap-3 items-center profile col-span-3 py-4 box-border secondary-bg drop-shadow-lg rounded'>
+      <div className='profile-row grid grid-cols-12 md:grid-cols-11 my-5 gap-x-5'>
+        <div className='w-auto flex jusitfy-center flex-col gap-3 items-center profile col-span-4 md:col-span-3 py-4 box-border secondary-bg drop-shadow-lg rounded'>
           <div className='profileImg'>
-            <img src={profileImg} width={150} height={150} alt='' />
+            <img
+              className='w-20 md:w-32 h-20 md:h-32'
+              src={profileImg}
+              alt=''
+            />
           </div>
-          <button className='px-5 uppercase py-1 w-auto text-xs border border-blue-600 rounded hover:bg-blue-600 hover:text-white'>
+          <button className=' w-max p-2 md:px-5 md:uppercase py-1 w-auto text-xs border border-blue-600 rounded hover:bg-blue-600 hover:text-white'>
             Change Profile Photo
           </button>
-          <p className=' text-xs'>
+          <p className=' text-xs text-center'>
             Registration Date: <span className='font-bold'>20 June,2020</span>
           </p>
         </div>
-        <div className='profile col-span-7 secondary-bg px-2 drop-shadow-lg rounded'>
-          profile Data
+        <div className='profile col-span-8 md:col-span-7 secondary-bg px-2 drop-shadow-lg rounded p-4'>
+          <div className='dataHeader flex justify-between px-3 mb-5'>
+            <p>Personal Information</p>
+            <button className='px-5 uppercase py-1 w-auto text-xs border border-blue-600 rounded hover:bg-blue-600 hover:text-white'>
+              Edit
+            </button>
+          </div>
+          <div className='personal-details grid grid-cols-2 md:grid-cols-3 px-3 gap-5'>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>Name</p>
+              <p className='text-sm '>Abdul Samad Khan</p>
+            </div>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>University Name</p>
+              <p className='text-sm '>University of Zambia</p>
+            </div>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>Program of Study</p>
+              <p className='text-sm '>Bachelor's Student</p>
+            </div>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>Phone Number</p>
+              <p className='text-sm '>+92 336 0097 019</p>
+            </div>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>Parent's/Guardian</p>
+              <p className='text-sm '>Sabz Ali Khan</p>
+            </div>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>Nationality</p>
+              <p className='text-sm '>Pakistani</p>
+            </div>
+            <div className='data-item'>
+              <p className='text-xs text-gray-400'>Address</p>
+              <p className='text-sm '>Bannu, KPK Pakistan</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
