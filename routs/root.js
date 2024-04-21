62f64f349cebf0adad894e86db0d4eb5bc8c6be1
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-04-21 10:21:00 +0500

    header updated

--- routs/root.js
+++ routs/root.js
@@ -1,17 +1,7 @@
-import { lazy } from 'react';
 import AuthLayout from '../src/layout/AuthLayout';
 import DashboardLayout from '../src/layout/DashbordLayout';
 import Chat from '../src/pages/Chat';
 import User from '../src/pages/User';
-
-// const Login = lazy(() => import('../pages/auth/login'));
-// const Register = lazy(() => import('../pages/auth/Register'));
-// const Dashboard = lazy(() => import('../pages/dashboard'));
-// const Products = lazy(() => import('../pages/product/product'));
-// const AllUsers = lazy(() => import('../pages/users/alluser'));
-// const MapView = lazy(() => import('../pages/mapview'));
-// const Calender = lazy(() => import('../pages/calender'));
-// const Profile = lazy(() => import('../pages/profile'));
 import Dashboard from '../src/pages/Dashboard';
 import LoginPage from '../src/pages/LoginPage';
 import Help from '../src/pages/Help';
@@ -19,44 +9,44 @@ import Help from '../src/pages/Help';
 // import DashboardLayout from '../layouts/DashboardLayout';
 
 export const routesConfig = [
-	{
-		path: '/',
-		component: LoginPage,
-		layout: AuthLayout,
-	},
-	// {
-	// 	path: '/auth/register',
-	// 	component: Register,
-	// 	layout: AuthLayout,
-	// },
-	{
-		path: '/dashboard',
-		component: Dashboard,
-		layout: DashboardLayout,
-	},
-	{
-		path: '/chat',
-		component: Chat,
-		layout: DashboardLayout,
-	},
-	{
-		path: '/user',
-		component: User,
-		layout: DashboardLayout,
-	},
-	{
-		path: '/help',
-		component: Help,
-		layout: DashboardLayout,
-	},
-	// {
-	// 	path: '/calender',
-	// 	component: Calender,
-	// 	layout: DashboardLayout,
-	// },
-	// {
-	// 	path: '/profile',
-	// 	component: Profile,
-	// 	layout: DashboardLayout,
-	// },
+  {
+    path: '/',
+    component: LoginPage,
+    layout: AuthLayout,
+  },
+  // {
+  // 	path: '/auth/register',
+  // 	component: Register,
+  // 	layout: AuthLayout,
+  // },
+  {
+    path: '/dashboard',
+    component: Dashboard,
+    layout: DashboardLayout,
+  },
+  {
+    path: '/chat',
+    component: Chat,
+    layout: DashboardLayout,
+  },
+  {
+    path: '/user',
+    component: User,
+    layout: DashboardLayout,
+  },
+  {
+    path: '/help',
+    component: Help,
+    layout: DashboardLayout,
+  },
+  // {
+  // 	path: '/calender',
+  // 	component: Calender,
+  // 	layout: DashboardLayout,
+  // },
+  // {
+  // 	path: '/profile',
+  // 	component: Profile,
+  // 	layout: DashboardLayout,
+  // },
 ];
--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,79 @@
-// import { useState } from 'react';
-// import React from 'react';
-// import LoginPage from './pages/LoginPage';
-// import { Outlet } from 'react-router-dom';
-// import {
-// 	faHtml5,
-// 	faCss3,
-// 	faBootstrap,
-// 	faReact,
-// } from '@fortawesome/free-brands-svg-icons';
-// import Sidebar from './components/Sidebar';
-// import Header from './components/Header';
+// // import { useState } from 'react';
+// // import React from 'react';
+// // import LoginPage from './pages/LoginPage';
+// // import { Outlet } from 'react-router-dom';
+// // import {
+// // 	faHtml5,
+// // 	faCss3,
+// // 	faBootstrap,
+// // 	faReact,
+// // } from '@fortawesome/free-brands-svg-icons';
+// // import Sidebar from './components/Sidebar';
+// // import Header from './components/Header';
 
-// function App() {
+// // function App() {
+// // 	return (
+// // 		<>
+// // 			<div className='grid grid-cols-12'>
+// // 				<Sidebar />
+// // 				<Header />
+// // 				<Outlet />
+// // 			</div>
+// // 		</>
+// // 	);
+// // }
+
+// // export default App;
+// import React, { Suspense } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import { routesConfig } from '../routs/root';
+
+// // import './App.css';
+// // import { routesConfig } from '';
+
+// const App = () => {
 // 	return (
-// 		<>
-// 			<div className='grid grid-cols-12'>
-// 				<Sidebar />
-// 				<Header />
-// 				<Outlet />
-// 			</div>
-// 		</>
+// 		<Routes>
+// 			{routesConfig.map(route => (
+// 				<Route
+// 					key={route.path}
+// 					path={route.path}
+// 					element={
+// 						<route.layout>
+// 							<route.component />
+// 						</route.layout>
+// 					}
+// 				/>
+// 			))}
+// 		</Routes>
 // 	);
-// }
+// };
 
 // export default App;
-import React, { Suspense } from 'react';
+// App.jsx
+
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routesConfig } from '../routs/root';
-
-// import './App.css';
-// import { routesConfig } from '';
+// import DashboardLayout from '../layouts/DashboardLayout';
+// import AuthLayout from '../layouts/AuthLayout';
 
 const App = () => {
-	return (
-		<Routes>
-			{routesConfig.map(route => (
-				<Route
-					key={route.path}
-					path={route.path}
-					element={
-						<route.layout>
-							<route.component />
-						</route.layout>
-					}
-				/>
-			))}
-		</Routes>
-	);
+  return (
+    <Routes>
+      {routesConfig.map((route) => (
+        <Route
+          key={route.path}
+          path={route.path}
+          element={
+            <route.layout title={route.path}>
+              <route.component />
+            </route.layout>
+          }
+        />
+      ))}
+    </Routes>
+  );
 };
 
 export default App;
