54477dbeb30f83f0c5a516a139238c7e2c585937
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-10-06 21:01:42 +0500

    sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,81 +1,67 @@
 import {
-	faCircleLeft,
-	faCircleQuestion,
-	faMessage,
-	faRightToBracket,
-	faTableCellsLarge,
-	faUser,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import IMG from '../assets/images/profile.png';
+  faCircleLeft,
+  faCircleQuestion,
+  faMessage,
+  faTableCellsLarge,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import IMG from "../assets/images/profile.png";
 function Sidebar() {
-	return (
-		<div className='flex flex-col items-center justify-center text-white'>
-			<div className='mb-5'>
-				<h1 className='hidden sm:block text-3xl text-white font-bold'>
-					HELSB
-				</h1>
-			</div>
-			<div className=' hidden sm:flex flex-col items-center justify-center mb-5 mt-5'>
-				<img src={IMG} alt='' width={100} />
-				<h2 className='text-sm font-semibold my-2 hidden md:block'>
-					Abdul Samad Khan
-				</h2>
-			</div>
-			<div className='grid grid-cols-1 md:grid-cols-2 gap-3 mb-10'>
-				<Link to='/dashboard'>
-					<div className='icons p-2 '>
-						<FontAwesomeIcon
-							className='text-xl lg:text-lg'
-							icon={faTableCellsLarge}
-						/>
-						<p className='text-xs hidden lg:block  font-thin'>
-							Dashboard
-						</p>
-					</div>
-				</Link>
-				<Link to='/chat'>
-					<div className='icons p-2'>
-						<FontAwesomeIcon
-							className='text-xl lg:text-lg'
-							icon={faMessage}
-						/>
-						<p className='text-xs hidden lg:block  font-thin'>
-							Chat
-						</p>
-					</div>
-				</Link>
-				<Link to='/user'>
-					<div className='icons p-2'>
-						<FontAwesomeIcon
-							className='text-xl lg:text-lg'
-							icon={faUser}
-						/>
-						<p className='text-xs hidden lg:block  font-thin'>
-							User
-						</p>
-					</div>
-				</Link>
-				<Link to='/help'>
-					<div className='icons p-2'>
-						<FontAwesomeIcon
-							className='text-xl lg:text-lg'
-							icon={faCircleQuestion}
-						/>
-						<p className='text-xs hidden lg:block  font-thin'>
-							Help
-						</p>
-					</div>
-				</Link>
-			</div>
-			<div className='flex flex-col justify-center items-center mt-3'>
-				<FontAwesomeIcon className='text-3xl' icon={faCircleLeft} />
-				<p className='text-xs hidden lg:block  font-thin'>Log Out</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center text-white">
+      <div className="mb-5">
+        <h1 className="hidden sm:block text-3xl text-white font-bold">HELSB</h1>
+      </div>
+      <div className=" hidden sm:flex flex-col items-center justify-center mb-5 mt-5">
+        <img src={IMG} alt="" width={100} />
+        <h2 className="text-sm font-semibold my-2 hidden md:block">
+          Abdul Samad Khan
+        </h2>
+      </div>
+      <div
+        className="flex flex-col w-full
+			 gap-3 mb-10"
+      >
+        <Link to="/dashboard">
+          <div className="icons p-2 ">
+            <FontAwesomeIcon
+              className="text-xl lg:text-lg"
+              icon={faTableCellsLarge}
+            />
+            <p className="text-xs hidden lg:block  font-normal">Dashboard</p>
+          </div>
+        </Link>
+        <Link to="/chat">
+          <div className="icons p-2">
+            <FontAwesomeIcon className="text-xl lg:text-lg" icon={faMessage} />
+            <p className="text-xs hidden lg:block  font-normal">Chat</p>
+          </div>
+        </Link>
+        <Link to="/user">
+          <div className="icons p-2 hover:bg-[#4953cf]">
+            <FontAwesomeIcon className="text-xl lg:text-lg" icon={faUser} />
+            <p className="text-xs hidden lg:block  font-normal">User</p>
+          </div>
+        </Link>
+        <Link to="/help">
+          <div className="icons p-2">
+            <FontAwesomeIcon
+              className="text-xl lg:text-lg"
+              icon={faCircleQuestion}
+            />
+            <p className="text-xs hidden lg:block  font-normal">Help</p>
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-col justify-center items-center mt-3 cursor-pointer">
+        <FontAwesomeIcon className="text-3xl" icon={faCircleLeft} />
+        <p className="text-xs hidden lg:block  font-thin">Log Out</p>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/layout/DashbordLayout.jsx
+++ src/layout/DashbordLayout.jsx
@@ -1,58 +1,17 @@
-// // import DashboardHeader from '../component/Header/DashboardHeader';
-// import React from 'react';
-// // import Bigsidebar from '../component/sidebars/bigsidebar';
-// // import SmallSidebar from '../component/sidebars/smallSidebar';
-// // import { MdCancel } from 'react-icons/md';
-// // import { GiHamburgerMenu } from 'react-icons/gi';
-// import './newCss.css';
-// // import { useWindowSize } from 'react-use';
-// import Header from '../components/Header';
-// import Sidebar from '../components/Sidebar';
-// const DashboardLayout = ({ children }) => {
-//   // const [isBigsidebar, setBigsidebar] = React.useState(true);
-//   // const handleToggle = () => {
-//   // 	setBigsidebar(prev => !prev);
-//   // };
-//   // const { width } = useWindowSize();
-//   // React.useEffect(() => {
-//   // 	if (width < 850) {
-//   // 		setBigsidebar(false);
-//   // 	} else {
-//   // 		setBigsidebar(true);
-//   // 	}
-//   // }, [width]);
-
-//   return (
-//     <div className='grid grid-cols-12 '>
-//       <div className='col-span-2 bg-primary h-screen flex justify-center'>
-//         <Sidebar />
-//       </div>
-
-//       <main className=' flex flex-col col-span-10 h-full overflow-auto '>
-//         <Header />
-//         <div className='w-full '>{children}</div>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default DashboardLayout;
-// DashboardLayout.js
-
-import React from 'react';
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
+import React from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = ({ children, title }) => {
   return (
-    <div className='grid grid-cols-12'>
-      <div className='col-span-2 bg-primary h-screen flex justify-center'>
+    <div className="grid grid-cols-12">
+      <div className="col-span-2 bg-primary h-full flex justify-center ">
         <Sidebar />
       </div>
 
-      <main className=' flex flex-col col-span-10 h-full overflow-auto'>
+      <main className=" flex flex-col col-span-10 h-full overflow-auto">
         <Header title={title} />
-        <div className='w-full'>{children}</div>
+        <div className="w-full">{children}</div>
       </main>
     </div>
   );
